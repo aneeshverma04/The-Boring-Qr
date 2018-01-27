a35f3ed0d5676f49412feeea6806bb4a9f7097fb
aneeshverma04 <aneeshvrma@yahoo.com>
2018-01-28 00:22:04 +0530

    Got rid of generator.java & QR generated at mainActivity

--- app/src/main/java/irobinz/tk/TheBoringQR/MainActivity.java
+++ app/src/main/java/irobinz/tk/TheBoringQR/MainActivity.java
@@ -3,20 +3,21 @@ package irobinz.tk.TheBoringQR;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import net.glxn.qrgen.android.QRCode;
+import net.glxn.qrgen.core.scheme.VCard;
 
 /*
 * Both buttons with layout of Wrap_Content... so it grows and shrinks accordingly
@@ -42,27 +43,16 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this); */
 
-        /*
-        * Button1 : Generate QR
-        * */
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-                                                         @Override
-                                                         public void onClick(View v) {
-                                                             Button btn = (Button)v;
-                                                             btn.setText("Naya Don");
-                                                             startActivity(new Intent(MainActivity.this,Generator.class));
-                                                         }
-                                                     }
-        );
+
         findViewById(R.id.formBtn).setOnClickListener(new View.OnClickListener() {
                                                          @Override
                                                          public void onClick(View v) {
-                                                             Button btn = (Button)v;
-                                                             btn.setText("Pressed");
-                                                             startActivity(new Intent(MainActivity.this,contact_form.class));
-                                                         }
-                                                     }
-        );
+            Button btn = (Button)v;
+            btn.setText("Pressed");
+            startActivity(new Intent(MainActivity.this,contact_form.class));
+                }
+                    }
+            );
 
 
         /*
@@ -88,6 +78,8 @@ public class MainActivity extends AppCompatActivity {
                 tv.setText("Whos your daddy");
             }
         });
+
+        GenerateQR();
     }
 
     @Override
@@ -115,29 +107,19 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    //@SuppressWarnings("StatementWithEmptyBody")
-    /*
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+    private void GenerateQR() {
+        ImageView imageView = findViewById(R.id.imageView2);
+        VCard vCard = getVCard();
+        Bitmap bitmap = QRCode.from(vCard).bitmap();
+        imageView.setImageBitmap(bitmap);
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }*/
+    }
+    private VCard getVCard() {
+        VCard vcard = new VCard();
+        vcard.setName("Robin");
+        vcard.setAddress("Sector 9");
+        vcard.setPhoneNumber("9815540544");
+        return vcard;
+    }
 }
